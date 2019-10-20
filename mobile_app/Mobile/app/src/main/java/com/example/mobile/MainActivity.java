dc12088ebf7bff2ad205958c4cb478e8796f498d
Brendan Leung <brendanleung@brendans-mbp.home>
2019-10-20 17:04:14 +1100

    get GPS coordinates for device

--- mobile_app/Mobile/app/src/main/java/com/example/mobile/MainActivity.java
+++ mobile_app/Mobile/app/src/main/java/com/example/mobile/MainActivity.java
@@ -1,18 +1,37 @@
 package com.example.mobile;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int LOCATION_REQUEST_CODE = 1;
+
+    private FusedLocationProviderClient client;
+    private boolean locationPermissionGranted;
+    LocationCallback locationCallback;
+    LocationRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+        locationPermissionGranted = false;
+
+        // Get location permission
+        getLocationPermission();
+
+        // checks location periodically and sends "checkin"
+        checkLocation();
     }
 
     @Override
@@ -51,5 +79,74 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /*
+    Get location permission for device
+     */
+    private void getLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            locationPermissionGranted = true;
+
+        } else {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+        }
+    }
+
+    /*
+    Handles the result of the permission request
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE: {
+
+                // if permission is not granted
+                if (grantResults.length > 0
+                        && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast exitAppToast = Toast.makeText(MainActivity.this,
+                            "Need to grant location permissions to use app", Toast.LENGTH_LONG);
+                    exitAppToast.show();
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+
+    /*
+    Requests location and sends "checkin" to server
+     */
+    private void checkLocation() {
+
+        request = LocationRequest.create();
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        try {
+            if (locationPermissionGranted == true) {
+                client.requestLocationUpdates(request, locationCallback = new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        Location location = locationResult.getLastLocation();
+                        if (location != null) {
+                            // CHECK LOCATION AND SEND "CHECKIN"
+                            TextView textView = findViewById(R.id.location);
+                            textView.setText("latitude: " + location.getLatitude() +
+                                    "\n longitude: " + location.getLongitude());
+                        }
+                    }
+                }, null);
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
     }
 }
